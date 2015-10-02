0710bd77a7b70a0a43c4b6d5e434c30e67e8bf50
Yu-han Lin <guesslin1986@gmail.com>
2015-10-02 15:35:25 +0800

    Alter: handle connection with ncat
    	handle connection with nmap/ncat
    	max connection set to 30
    	ropme update with several function

--- Dockerfile
+++ Dockerfile
@@ -2,16 +2,16 @@ FROM ubuntu:14.04
 MAINTAINER guesslin1986@gmail.com
 
 RUN apt-get update && \
-    apt-get install -y build-essential gcc execstack
+    apt-get install -y build-essential gcc execstack nmap
 
 
 COPY ["./ropme", "/opt/ropme/"]
+COPY ["./flag", "/opt/flag"]
 
 WORKDIR /opt/ropme/
 
 RUN make clean dep all
-RUN mkfifo pipe
 
 EXPOSE 5555
 
-ENTRYPOINT nc -l -p 5555 < pipe | setarch `arch` -R /opt/ropme/ropme >pipe
+ENTRYPOINT ncat -k -m 30 -l -e "/usr/bin/setarch x86_64 -R /opt/ropme/ropme" 5555
--- ropme/main.c
+++ ropme/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h> 
+#include <stdlib.h>
 
-int banner();
+void banner();
+void commands(char *);
+void get_address(char *);
 int ropme();
-int welcome();
+void show_commands();
 
 int main(void) { 
 	banner();
@@ -10,16 +13,7 @@ int main(void) {
 	return 0; 
 }
 
-int ropme() {
-	char name[64];
-	printf("%p\n", name); // print the buffer base address
-	puts("What's your name?");
-	gets(name);
-	printf("Hello, %s!\n", name);
-	return 0;
-}
-
-int banner() {
+void banner() {
 	printf("\n");
 	printf("######                                   \n");
 	printf("#     #  ####  #####        #    # ######\n");
@@ -29,4 +23,49 @@ int banner() {
 	printf("#    #  #    # #            #    # #     \n");
 	printf("#     #  ####  #            #    # ######\n");
 	printf("\n");
+	fflush(stdout);
+}
+
+void commands(char *name) {
+	char select[64];
+	int option;
+	while(1) {
+		show_commands();
+		fgets(select, 64, stdin);
+		option = atoi(select);
+		switch(option){
+		case 1:
+			get_address(name); // print the buffer base address
+			break;
+		case 2:
+			printf("The flag located at /opt/flag \n");
+			break;
+		case 3:
+			return;
+		default:
+			break;
+		}
+	}
+}
+
+void get_address(char *cptr) {
+	printf("%p\n", cptr);
+}
+
+int ropme() {
+	char name[64];
+	puts("What's your name?");
+	fflush(stdout);
+	gets(name);
+	printf("Hello, %s!\n", name);
+	commands(name);
+	return 0;
+}
+
+void show_commands() {
+	printf("1) I want to stand\n");
+	printf("2) show me the flag\n");
+	printf("3) exit.\n");
+	printf("Please choose command: ");
+	fflush(stdout);
 }
